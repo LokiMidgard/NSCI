0e9d87a63c782ef7c9d5512c173f5e5aeef64906
Patrick Kranz <patrick-kranz@live.de>
2017-06-05 14:04:19 +0200

    Changed Remove so it now should find the to delte item faster.

--- NSCI/DepthComparer.cs
+++ NSCI/DepthComparer.cs
@@ -26,6 +26,46 @@ namespace NSCI
             base.InsertItem(insertIndex, item);
         }
 
+        public new bool Remove(T item)
+        {
+            var foundIndex = GetInsertIndex(item);
+            if (foundIndex < 0) // No element found
+                return false;
+
+            //we found one Item that has the same Size, but this must not be the equals to the searched object.
+
+            if (item.Equals(this[foundIndex]))
+            {
+                RemoveAt(foundIndex);
+                return true;
+            }
+
+
+            var i = foundIndex - 1;
+            //search backwards
+            while (i >= 0 && !item.Equals(this[i]) && this.comparer.Compare(item, this[i]) == 0)
+                --i;
+
+            if (i >= 0 && item.Equals(this[i]))
+            {
+                RemoveAt(i);
+                return true;
+            }
+
+            i = foundIndex + 1;
+            //search forward
+            while (i < Count && !item.Equals(this[i]) && this.comparer.Compare(item, this[i]) == 0)
+                ++i;
+
+            if (i < Count && item.Equals(this[i]))
+            {
+                RemoveAt(i);
+                return true;
+            }
+
+            return false;
+        }
+
         private int GetInsertIndex(T item)
         {
             if (Count == 0)
