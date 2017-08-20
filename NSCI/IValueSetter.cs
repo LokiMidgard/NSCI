0532c094c560cf46b0b7dbf9de7773710647dd3e
Patrick Kranz <patrick-kranz@live.de>
2017-08-20 14:20:05 +0200

    Renamed Instanciator to Template
    also added Support for instatntication of objext trees.

---
+++ NSCI/IValueSetter.cs
@@ -0,0 +1,17 @@
+﻿using NDProperty.Propertys;
+
+namespace NSCI
+{
+    public interface ITemplateSetter<TValue, TType, TProperty> : ITemplateSetter<TType>
+        where TType : class
+        where TProperty : NDReadOnlyPropertyKey<NDPConfiguration, TValue, TType>, INDProperty<NDPConfiguration, TValue, TType>
+    {
+        TProperty Property { get; }
+        TValue Value { get; }
+    }
+    public interface ITemplateSetter<in TType>
+        where TType : class
+    {
+        void SetValueOnObject(TType obj);
+    }
+}
--- NSCI/Instaciator.cs
+++ NSCI/Instaciator.cs
@@ -4,20 +4,57 @@ using System.Collections.Generic;
 using System.Text;
 using NSCI.UI.Controls;
 using System.Linq;
+using NDProperty.Propertys;
+using NDProperty;
 
 namespace NSCI
 {
-    public class Instaciator<TType> where TType : class, new()
+    public interface ITemplate<out TType> where TType : class, new()
     {
-        public IEnumerable<ISetter<TType>> Setter { get; set; }
+        TType InstanciateObject();
+    }
+    public class Template<TType> :ITemplate<TType>
+        where TType : class, new()
+    {
+        public IEnumerable<ITemplateSetter<TType>> Setter { get; set; }
         public TType InstanciateObject()
         {
             var obj = new TType();
 
             foreach (var item in Setter)
-                item.SetOnObject(obj);
+                item.SetValueOnObject(obj);
             return obj;
         }
 
+    }
+
+    public class TemplateSetter<TValue, TType, TProperty> : ITemplateSetter<TValue, TType, TProperty>
+        where TType : class
+        where TValue : class, new()
+        where TProperty : NDReadOnlyPropertyKey<NDPConfiguration, TValue, TType>, INDProperty<NDPConfiguration, TValue, TType>
+    {
+        public TProperty Property { get; set; }
+
+        public ITemplate<TValue> ValueTemplate { get; set; }
+
+        public TValue Value => ValueTemplate.InstanciateObject();
+
+        public void SetValueOnObject(TType obj)
+        {
+            PropertyRegistar<NDPConfiguration>.SetValue(Property, obj, Value);
+        }
+    }
+    public class ValueSetter<TValue, TType, TProperty> : ITemplateSetter<TValue, TType, TProperty>
+        where TType : class
+        where TProperty : NDReadOnlyPropertyKey<NDPConfiguration, TValue, TType>, INDProperty<NDPConfiguration, TValue, TType>
+    {
+        public TProperty Property { get; set; }
+
+        public TValue Value { get; set; }
+
+        public void SetValueOnObject(TType obj)
+        {
+            PropertyRegistar<NDPConfiguration>.SetValue(Property, obj, Value);
+        }
     }
 }
