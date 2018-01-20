568f845d68b6fee8aa02f6fc1f920c3bbfb8af55
Patrick Kranz <patrick-kranz@live.de>
2018-01-20 21:08:03 +0100

    ...

--- NSCI/UI/Controls/ItemsControl.cs
+++ NSCI/UI/Controls/ItemsControl.cs
@@ -3,54 +3,80 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
-
+using NDProperty;
+using NDProperty.Propertys;
+using NSCI.Propertys;
+using NSCI.UI.Controls.Layout;
 
 namespace NSCI.UI.Controls
 {
-    //public class ItemsControl : Control
-    //{
-    //    public ObservableCollection<UIElement> Items { get; } = new ObservableCollection<UIElement>();
+    public partial class ItemsControl : Control
+    {
 
-    //    public ItemsControl()
-    //    {
-    //        this.Items.CollectionChanged += (s, e) =>
-    //        {
-    //            this.InvalidateArrange();
-    //            if (e.NewItems != null)
-    //                foreach (UIElement uielement in e.NewItems)
-    //                    uielement.VisualParent = this;
-    //            if (e.OldItems != null)
-    //                foreach (UIElement uielement in e.OldItems)
-    //                    uielement.VisualParent = null;
-    //        };
-    //    }
+        public ItemsControl()
+        {
+            DisplayContentChanged += ContentControl_DisplayContentChanged;
+        }
 
-    //    protected override Size MeasureOverride(Size availableSize)
-    //    {
-    //        availableSize = base.MeasureOverride(availableSize);
-    //        foreach (var item in Items)
-    //            item.Measure(availableSize);
+        private void ContentControl_DisplayContentChanged(object sender, ChangedEventArgs<NDPConfiguration, Control, UIElement> e)
+        {
+            if (e.NewValue != null)
+            {
+                var queue = new Queue<UIElement>();
+                queue.Enqueue(e.NewValue);
 
-    //        var desiredWidth = Items.Max(x => x.DesiredSize.Width);
-    //        var desiredHeight = Items.Max(x => x.DesiredSize.Height);
-    //        return new Size(desiredWidth, desiredHeight);
-    //    }
+                while (queue.Count != 0)
+                {
+                    var element = queue.Dequeue();
+                    if (element is ItemsPresenter p)
+                    {
+                        ItemsPresenter = p;
+                        return;
+                    }
+                    foreach (var child in element.VisualChildrean)
+                        queue.Enqueue(child);
+                }
+                ItemsPresenter = null;
 
-    //    protected override void ArrangeOverride(Size finalSize)
-    //    {
-    //        base.ArrangeOverride(finalSize);
-    //        foreach (var item in Items)
-    //            item.Arrange(new Rect(Point.Empty, finalSize));
-    //    }
+            }
+        }
 
-    //    protected override void RenderOverride(IRenderFrame frame)
-    //    {
-    //        foreach (var item in Items)
-    //        {
-    //            var location = GetLocation(item);
-    //            item.Render(frame.GetGraphicsBuffer(location));
-    //        }
-    //    }
+        [NDProperty.NDP]
+        protected virtual void OnItemsPresenterChanging(NDProperty.Propertys.OnChangingArg<NDPConfiguration, ItemsPresenter> arg)
+        {
+            if (arg.Property.IsObjectValueChanging)
+                arg.ExecuteAfterChange += (sender, args) => UpdateItemsPresenter();
+        }
 
-    //}
+        private void UpdateItemsPresenter()
+        {
+            if (ItemsPresenter != null)
+            {
+                foreach (var item in this.Items)
+                    ItemsPresenter.Add(item);
+            }
+
+        }
+
+        [NDP]
+        protected void OnPanelTemplateChanging(NDProperty.Propertys.OnChangingArg<Propertys.NDPConfiguration, IControlTemplate<Panel, ItemsControl>> arg)
+        {
+
+        }
+
+        [NDP]
+        protected void OnItemsTemplateChanging(NDProperty.Propertys.OnChangingArg<Propertys.NDPConfiguration, IControlTemplate<Panel, ItemsControl>> arg)
+        {
+
+        }
+
+        [NDP]
+        protected void OnItemsChanging(NDProperty.Propertys.OnChangingArg<Propertys.NDPConfiguration, IEnumerable<object>> arg)
+        {
+
+        }
+
+
+
+    }
 }
---
+++ NSCI/UI/Controls/ItemsPresenter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NDProperty;
+using NSCI.UI.Controls.Layout;
+
+namespace NSCI.UI.Controls
+{
+    public partial class ItemsPresenter : FrameworkElement
+    {
+
+        [NDP]
+        private void OnPanelChanging(NDProperty.Propertys.OnChangingArg<Propertys.NDPConfiguration, Panel> arg)
+        {
+
+        }
+
+        internal void Add(object item)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Clear()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
